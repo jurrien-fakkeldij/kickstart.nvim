14e7863d27f5208ee152c3c0994c8eef125eeffd
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2025-04-09 22:59:29 +0200

    feat: add refactor plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -72,4 +72,30 @@ return {
       require 'custom.plugins.config.fugitive'
     end,
   },
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    lazy = false,
+    opts = {},
+    config = function()
+      require('refactoring').setup {
+        show_success_message = false,
+      }
+      vim.keymap.set('x', '<leader>re', ':Refactor extract ', { desc = 'Refactor extract' })
+      vim.keymap.set('x', '<leader>rf', ':Refactor extract_to_file ', { desc = 'Refactor extract to file' })
+      vim.keymap.set('x', '<leader>rv', ':Refactor extract_var ', { desc = 'Refactor extract variable' })
+      vim.keymap.set({ 'n', 'x' }, '<leader>ri', ':Refactor inline_var', { desc = 'Refactor extract inline variable' })
+      vim.keymap.set('n', '<leader>rI', ':Refactor inline_func', { desc = 'Refactor inline function' })
+      vim.keymap.set('n', '<leader>rb', ':Refactor extract_block', { desc = 'Refactor extract block' })
+      vim.keymap.set('n', '<leader>rbf', ':Refactor extract_block_to_file', { desc = 'Refactor extract block to file' })
+      -- prompt for a refactor to apply when the remap is triggered
+      vim.keymap.set({ 'n', 'x' }, '<leader>rr', function()
+        require('refactoring').select_refactor()
+      end, { desc = 'Refactor select option' })
+      -- Note that not all refactor support both normal and visual mode
+    end,
+  },
 }
